b7a9feba1cf72b8046759e580a3c56e1a83ad94b
Jadyn <33948021+meljadyn@users.noreply.github.com>
2022-08-07 12:07:29 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import { Route, Switch, NavLink, Redirect } from "react-router-dom";
+import Home from "./components/Home";
+import Stocks from "./components/Stocks";
+import Movies from "./components/Movies";
+
+function App() {
+  return (
+    <div className="main">
+      <h1>App Component</h1>
+      <nav className="comp nav">
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              exact
+              activeClassName="purple"
+              activeStyle={{ fontWeight: "bold" }}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/stocks"
+              activeClassName="purple"
+              activeStyle={{ fontWeight: "bold" }}
+            >
+              Stocks
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/movies"
+              activeClassName="purple"
+              activeStyle={{ fontWeight: "bold" }}
+            >
+              Movies
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/stocks">
+          <Stocks />
+        </Route>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route path="/not-logged-in">
+          <h1>You must be logged in to enter</h1>
+        </Route>
+        <Route>
+          <h1>Page Not Found</h1>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Stocks/index.js
@@ -0,0 +1,21 @@
+import { Redirect, useHistory } from "react-router-dom";
+
+function Stocks() {
+  let loggedIn = true;
+  if (!loggedIn) return <Redirect to="/not-logged-in" />;
+
+  const history = useHistory();
+
+  const handleClick = () => {
+    window.alert("Sending info to the DB!");
+    history.push("/");
+  };
+  return (
+    <div className="comp orange">
+      <h1>Stocks Component</h1>
+      <button onClick={handleClick}>Home</button>
+    </div>
+  );
+}
+
+export default Stocks;
